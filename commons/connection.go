5da8bf27c774203b6222c92bbc1a1947b2130558
David Velez <davidveleztorres96@gmail.com>
2021-08-07 13:27:03 -0500

    first commit comodels,controller

---
+++ commons/connection.go
@@ -0,0 +1,30 @@
+package commons
+
+import (
+	"log"
+
+	"github.com/cristhiandavid96/ServerGolang/models"
+
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func GetConnection() *gorm.DB {
+	db, error := gorm.Open("mysql", "root:@/farmacia?charset=utf8")
+
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	return db
+}
+
+func Migrate() {
+	db := GetConnection()
+	defer db.Close()
+
+	log.Println("Migrando....")
+
+	db.AutoMigrate(&models.Persona{})
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module githup.com/cristhiandavid96/ServerGolang
+
+go 1.16
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+)
